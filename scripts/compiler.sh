b4d56f5b2fdc548ed2e2838f9269d9a8bd2ba7d2
RealOrko <realorko@nowhere.com>
2025-03-01 19:13:58 +0000

    Added nasm compilation steps to script

--- scripts/compiler.sh
+++ scripts/compiler.sh
@@ -11,3 +11,7 @@ valgrind --leak-check=full \
          --verbose \
          --log-file=bin/valgrind-out.txt \
          bin/sn samples/hello-world/main.sn -o bin/hello-world.asm -l 3
+
+nasm bin/hello-world.asm -o bin/hello-world.o
+ld bin/hello-world.o -o bin/hello-world
+bin/hello-world
