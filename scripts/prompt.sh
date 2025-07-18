9b4f55680dad7714eb664d07a0dccb9649884bab
RealOrko <realorko@nowhere.com>
2025-07-18 20:51:36 +0100

    Updating prompts

--- scripts/prompt.sh
+++ scripts/prompt.sh
@@ -6,6 +6,7 @@ export SAMPLES_INPUT_CODE=$(cat samples/hello-world/simple.sn)
 export COMPILER_MAKE_FILE=$(cat compiler/Makefile)
 export SCRIPTS_COMPILER_SCRIPT=$(cat scripts/compiler.sh)
 export LOG_MAKE_OUTPUT=$(cat log/make-output.log)
+export LOG_COMPILER_OUTPUT=$(cat log/compiler-output.log)
 export PROMPT_TEMPLATE=$(cat scripts/prompt.template)
 
 cat scripts/prompt.template | envsubst > bin/prompt.txt
