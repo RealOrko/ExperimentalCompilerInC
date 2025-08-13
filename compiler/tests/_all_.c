291c842b200be1862320f020c8372010cc5c17bd
realorko <realorko@nowhere.com>
2025-08-13 17:17:41 +0100

    Reducing log level for tests

--- compiler/tests/_all_.c
+++ compiler/tests/_all_.c
@@ -8,7 +8,7 @@ int main()
 
     // *** Debugging ***
     printf("Running tests with debug level: %d\n", DEBUG_LEVEL_VERBOSE);
-    init_debug(DEBUG_LEVEL_INFO);
+    init_debug(DEBUG_LEVEL_ERROR);
 
     // *** Arena ***
 
