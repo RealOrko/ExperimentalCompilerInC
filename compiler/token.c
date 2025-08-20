352c6017e40e9a41cba5ab5617c3719c125dfc07
realorko <realorko@nowhere.com>
2025-08-20 20:11:50 +0100

    Making sure tokens are debuggable

--- compiler/token.c
+++ compiler/token.c
@@ -6,16 +6,23 @@
 
 void token_init(Token *token, TokenType type, const char *start, int length, int line, const char *filename)
 {
+    DEBUG_VERBOSE("Entering token_init: type=%s, start='%.*s', length=%d, line=%d, filename='%s'",
+                  token_type_to_string(type), length, start, length, line, filename ? filename : "NULL");
+
     token->type = type;
     token->start = start;
     token->length = length;
     token->line = line;
     token->literal.int_value = 0;
     token->filename = filename;
+
+    DEBUG_VERBOSE("Exiting token_init");
 }
 
 int token_is_type_keyword(TokenType type)
 {
+    DEBUG_VERBOSE("Entering token_is_type_keyword: type=%s", token_type_to_string(type));
+
     switch (type)
     {
     case TOKEN_INT:
@@ -25,199 +32,308 @@ int token_is_type_keyword(TokenType type)
     case TOKEN_STR:
     case TOKEN_BOOL:
     case TOKEN_VOID:
+        DEBUG_VERBOSE("Exiting token_is_type_keyword: returning 1");
         return 1;
     default:
+        DEBUG_VERBOSE("Exiting token_is_type_keyword: returning 0");
         return 0;
     }
 }
 
 TokenType token_get_array_token_type(TokenType base_type)
 {
+    DEBUG_VERBOSE("Entering token_get_array_token_type: base_type=%s", token_type_to_string(base_type));
+
+    TokenType result;
     switch (base_type)
     {
     case TOKEN_INT:
-        return TOKEN_INT_ARRAY;
+        result = TOKEN_INT_ARRAY;
+        break;
     case TOKEN_LONG:
-        return TOKEN_LONG_ARRAY;
+        result = TOKEN_LONG_ARRAY;
+        break;
     case TOKEN_DOUBLE:
-        return TOKEN_DOUBLE_ARRAY;
+        result = TOKEN_DOUBLE_ARRAY;
+        break;
     case TOKEN_CHAR:
-        return TOKEN_CHAR_ARRAY;
+        result = TOKEN_CHAR_ARRAY;
+        break;
     case TOKEN_STR:
-        return TOKEN_STR_ARRAY;
+        result = TOKEN_STR_ARRAY;
+        break;
     case TOKEN_BOOL:
-        return TOKEN_BOOL_ARRAY;
+        result = TOKEN_BOOL_ARRAY;
+        break;
     case TOKEN_VOID:
-        return TOKEN_VOID_ARRAY;
+        result = TOKEN_VOID_ARRAY;
+        break;
     default:
-        return base_type;
+        result = base_type;
+        break;
     }
+
+    DEBUG_VERBOSE("Exiting token_get_array_token_type: returning %s", token_type_to_string(result));
+    return result;
 }
 
 void token_set_int_literal(Token *token, int64_t value)
 {
+    DEBUG_VERBOSE("Entering token_set_int_literal: value=%ld", value);
+
     token->literal.int_value = value;
+
+    DEBUG_VERBOSE("Exiting token_set_int_literal");
 }
 
 void token_set_double_literal(Token *token, double value)
 {
+    DEBUG_VERBOSE("Entering token_set_double_literal: value=%f", value);
+
     token->literal.double_value = value;
+
+    DEBUG_VERBOSE("Exiting token_set_double_literal");
 }
 
 void token_set_char_literal(Token *token, char value)
 {
+    DEBUG_VERBOSE("Entering token_set_char_literal: value='%c'", value);
+
     token->literal.char_value = value;
+
+    DEBUG_VERBOSE("Exiting token_set_char_literal");
 }
 
 void token_set_string_literal(Token *token, const char *value)
 {
+    DEBUG_VERBOSE("Entering token_set_string_literal: value='%s'", value ? value : "NULL");
+
     token->literal.string_value = value;
+
+    DEBUG_VERBOSE("Exiting token_set_string_literal");
 }
 
 void token_set_bool_literal(Token *token, int value)
 {
+    DEBUG_VERBOSE("Entering token_set_bool_literal: value=%d", value);
+
     token->literal.bool_value = value;
+
+    DEBUG_VERBOSE("Exiting token_set_bool_literal");
 }
 
 const char *token_type_to_string(TokenType type)
 {
+    DEBUG_VERBOSE("Entering token_type_to_string: type=%d", type);
+
+    const char *result;
     switch (type)
     {
     case TOKEN_EOF:
-        return "EOF";
+        result = "EOF";
+        break;
     case TOKEN_INT_LITERAL:
-        return "INT_LITERAL";
+        result = "INT_LITERAL";
+        break;
     case TOKEN_LONG_LITERAL:
-        return "LONG_LITERAL";
+        result = "LONG_LITERAL";
+        break;
     case TOKEN_DOUBLE_LITERAL:
-        return "DOUBLE_LITERAL";
+        result = "DOUBLE_LITERAL";
+        break;
     case TOKEN_CHAR_LITERAL:
-        return "CHAR_LITERAL";
+        result = "CHAR_LITERAL";
+        break;
     case TOKEN_STRING_LITERAL:
-        return "STRING_LITERAL";
+        result = "STRING_LITERAL";
+        break;
     case TOKEN_INTERPOL_STRING:
-        return "INTERPOL_STRING";
+        result = "INTERPOL_STRING";
+        break;
     case TOKEN_BOOL_LITERAL:
-        return "BOOL_LITERAL";
+        result = "BOOL_LITERAL";
+        break;
     case TOKEN_IDENTIFIER:
-        return "IDENTIFIER";
+        result = "IDENTIFIER";
+        break;
     case TOKEN_FN:
-        return "FN";
+        result = "FN";
+        break;
     case TOKEN_VAR:
-        return "VAR";
+        result = "VAR";
+        break;
     case TOKEN_RETURN:
-        return "RETURN";
+        result = "RETURN";
+        break;
     case TOKEN_IF:
-        return "IF";
+        result = "IF";
+        break;
     case TOKEN_ELSE:
-        return "ELSE";
+        result = "ELSE";
+        break;
     case TOKEN_FOR:
-        return "FOR";
+        result = "FOR";
+        break;
     case TOKEN_WHILE:
-        return "WHILE";
+        result = "WHILE";
+        break;
     case TOKEN_IMPORT:
-        return "IMPORT";
+        result = "IMPORT";
+        break;
     case TOKEN_NIL:
-        return "NIL";
+        result = "NIL";
+        break;
     case TOKEN_INT:
-        return "INT";
+        result = "INT";
+        break;
     case TOKEN_LONG:
-        return "LONG";
+        result = "LONG";
+        break;
     case TOKEN_DOUBLE:
-        return "DOUBLE";
+        result = "DOUBLE";
+        break;
     case TOKEN_CHAR:
-        return "CHAR";
+        result = "CHAR";
+        break;
     case TOKEN_STR:
-        return "STR";
+        result = "STR";
+        break;
     case TOKEN_BOOL:
-        return "BOOL";
+        result = "BOOL";
+        break;
     case TOKEN_VOID:
-        return "VOID";
+        result = "VOID";
+        break;
     case TOKEN_INT_ARRAY:
-        return "INT_ARRAY";
+        result = "INT_ARRAY";
+        break;
     case TOKEN_LONG_ARRAY:
-        return "LONG_ARRAY";
+        result = "LONG_ARRAY";
+        break;
     case TOKEN_DOUBLE_ARRAY:
-        return "DOUBLE_ARRAY";
+        result = "DOUBLE_ARRAY";
+        break;
     case TOKEN_CHAR_ARRAY:
-        return "CHAR_ARRAY";
+        result = "CHAR_ARRAY";
+        break;
     case TOKEN_STR_ARRAY:
-        return "STR_ARRAY";
+        result = "STR_ARRAY";
+        break;
     case TOKEN_BOOL_ARRAY:
-        return "BOOL_ARRAY";
+        result = "BOOL_ARRAY";
+        break;
     case TOKEN_VOID_ARRAY:
-        return "VOID_ARRAY";
+        result = "VOID_ARRAY";
+        break;
     case TOKEN_PLUS:
-        return "PLUS";
+        result = "PLUS";
+        break;
     case TOKEN_MINUS:
-        return "MINUS";
+        result = "MINUS";
+        break;
     case TOKEN_STAR:
-        return "STAR";
+        result = "STAR";
+        break;
     case TOKEN_SLASH:
-        return "SLASH";
+        result = "SLASH";
+        break;
     case TOKEN_MODULO:
-        return "MODULO";
+        result = "MODULO";
+        break;
     case TOKEN_EQUAL:
-        return "EQUAL";
+        result = "EQUAL";
+        break;
     case TOKEN_EQUAL_EQUAL:
-        return "EQUAL_EQUAL";
+        result = "EQUAL_EQUAL";
+        break;
     case TOKEN_BANG:
-        return "BANG";
+        result = "BANG";
+        break;
     case TOKEN_BANG_EQUAL:
-        return "BANG_EQUAL";
+        result = "BANG_EQUAL";
+        break;
     case TOKEN_LESS:
-        return "LESS";
+        result = "LESS";
+        break;
     case TOKEN_LESS_EQUAL:
-        return "LESS_EQUAL";
+        result = "LESS_EQUAL";
+        break;
     case TOKEN_GREATER:
-        return "GREATER";
+        result = "GREATER";
+        break;
     case TOKEN_GREATER_EQUAL:
-        return "GREATER_EQUAL";
+        result = "GREATER_EQUAL";
+        break;
     case TOKEN_AND:
-        return "AND";
+        result = "AND";
+        break;
     case TOKEN_OR:
-        return "OR";
+        result = "OR";
+        break;
     case TOKEN_PLUS_PLUS:
-        return "PLUS_PLUS";
+        result = "PLUS_PLUS";
+        break;
     case TOKEN_MINUS_MINUS:
-        return "MINUS_MINUS";
+        result = "MINUS_MINUS";
+        break;
     case TOKEN_LEFT_PAREN:
-        return "LEFT_PAREN";
+        result = "LEFT_PAREN";
+        break;
     case TOKEN_RIGHT_PAREN:
-        return "RIGHT_PAREN";
+        result = "RIGHT_PAREN";
+        break;
     case TOKEN_LEFT_BRACE:
-        return "LEFT_BRACE";
+        result = "LEFT_BRACE";
+        break;
     case TOKEN_RIGHT_BRACE:
-        return "RIGHT_BRACE";
+        result = "RIGHT_BRACE";
+        break;
     case TOKEN_LEFT_BRACKET:
-        return "LEFT_BRACKET";
+        result = "LEFT_BRACKET";
+        break;
     case TOKEN_RIGHT_BRACKET:
-        return "RIGHT_BRACKET";
+        result = "RIGHT_BRACKET";
+        break;
     case TOKEN_SEMICOLON:
-        return "SEMICOLON";
+        result = "SEMICOLON";
+        break;
     case TOKEN_COLON:
-        return "COLON";
+        result = "COLON";
+        break;
     case TOKEN_COMMA:
-        return "COMMA";
+        result = "COMMA";
+        break;
     case TOKEN_DOT:
-        return "DOT";
+        result = "DOT";
+        break;
     case TOKEN_ARROW:
-        return "ARROW";
+        result = "ARROW";
+        break;
     case TOKEN_INDENT:
-        return "INDENT";
+        result = "INDENT";
+        break;
     case TOKEN_DEDENT:
-        return "DEDENT";
+        result = "DEDENT";
+        break;
     case TOKEN_NEWLINE:
-        return "NEWLINE";
+        result = "NEWLINE";
+        break;
     case TOKEN_ERROR:
-        return "ERROR";
+        result = "ERROR";
+        break;
     default:
-        return "INVALID";
+        result = "INVALID";
+        break;
     }
+
+    DEBUG_VERBOSE("Exiting token_type_to_string: returning '%s'", result);
+    return result;
 }
 
 void token_print(Token *token)
 {
+    DEBUG_VERBOSE("Entering token_print");
+
     char *text = malloc(token->length + 1);
     memcpy(text, token->start, token->length);
     text[token->length] = '\0';
@@ -252,4 +368,6 @@ void token_print(Token *token)
 
     DEBUG_VERBOSE(" }\n");
     free(text);
+
+    DEBUG_VERBOSE("Exiting token_print");
 }
